98f79fb3d5c6cb4b9406e51d1691ddb107f5c2b2
Alex Komissarov <k.a.komissar@gmail.com>
2019-10-11 23:59:40 +0500

    Generate array

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,14 @@ use rand::Rng;
 
 fn main() {
     let mut rng = rand::thread_rng();
-    let arr = (0..).map(|_x| rng.gen_range(0, 10)).map(f64::from);
+    let arr = (0..)
+        .map(|_x| rng.gen_range(0, 10))
+        .map(f64::from)
+        .take(10)
+        .collect::<Vec<_>>();
 
-    println!("{:?}", arr.take(10).collect::<Vec<_>>());
+    let size = arr.len();
+    let arr = arr.chunks(2).cycle().take(2 * size);
+
+    println!("{:?}", arr.collect::<Vec<_>>());
 }
