0f0e1945401b81fa2c2c3e966d1204a6628b4b0f
Alex Komissarov <k.a.komissar@gmail.com>
2019-10-11 23:10:26 +0500

    setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+extern crate rand;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+    let arr = (0..).map(|_x| rng.gen_range(0, 10)).map(f64::from);
+
+    println!("{:?}", arr.take(10).collect::<Vec<_>>());
 }
