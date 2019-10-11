2852bff3d6fedc98e88e66450852755bad33d204
Alex Komissarov <k.a.komissar@gmail.com>
2019-10-12 01:37:56 +0500

    +

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,27 @@ fn main() {
         .collect::<Vec<_>>();
 
     let size = arr.len();
-    let res = arr.chunks(2).cycle().take(size).map(|x| (x[0], x[1]));
+    let res = arr
+        .chunks(2)
+        .cycle()
+        .take(size)
+        .map(|x| (x[0], x[1]))
+        .collect::<Vec<_>>();
 
-    let res = res.enumerate().map(|(i, x)| {
-        // println!("{}: {}", i, (-1f64).powi((i / (size / 2)) as i32));
-        (x.0 + x.1 * (-1f64).powi((i / (size / 2)) as i32)) / 2.
-    });
+    let wave = |res: &Vec<(f64, f64)>, size: usize| {
+        res.iter()
+            .enumerate()
+            .map(|(i, x)| {
+                let frac = (-1f64).powi((i / (size / 2)) as i32);
+                (x.0 + x.1 * frac) / 2.
+            })
+            .collect::<Vec<_>>()
+    };
+
+    // for size in (0..size / 2 - 1).map(|n| size / (2 * n)).rev() {
+    // }
+    let res = wave(&res, size);
 
     println!("{:?}", arr);
-    println!("{:?}", res.collect::<Vec<_>>());
+    println!("{:?}", res);
 }
