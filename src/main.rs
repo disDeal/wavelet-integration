8fd4c5c8309b34015fcc348feca71ce706daf889
Alex Komissarov <k.a.komissar@gmail.com>
2019-10-12 14:30:24 +0500

    Reset

--- src/main.rs
+++ src/main.rs
@@ -17,52 +17,6 @@ fn main() {
     let size = arr.len();
 
     let res = arr.clone();
-    let factor = |n: usize| size / (2 * n);
-    let mean = |x: &[f64]| x[0] + x[1];
-    let div = |x: &[f64]| x[0] - x[1];
-
-    println!("Pairs:");
-    let pairs = res.chunks_exact(2).collect::<Vec<_>>();
-    println!("{:?}", pairs);
-
-    let group = |n: usize| pairs.chunks_exact(factor(n));
-
-    println!("Groups:");
-    println!("{:?}", group(1).collect::<Vec<_>>());
-    println!("{:?}", group(2).collect::<Vec<_>>());
-    println!("{:?}", group(3).collect::<Vec<_>>());
-
-    let wave = |n| {
-        let m = group(n).map(|arr| arr.iter().map(|x| mean(x)).collect::<Vec<_>>());
-        let d = group(n).map(|arr| arr.iter().map(|x| div(x)).collect::<Vec<_>>());
-
-        m.zip(d)
-    };
-
-    println!("Zips:");
-    println!("{:?}", wave(1).collect::<Vec<_>>());
-    println!("{:?}", wave(2).collect::<Vec<_>>());
-    println!("{:?}", wave(3).collect::<Vec<_>>());
-
-    let w3 = wave(2).collect::<Vec<_>>();
-
-    let t1 = w3
-        .into_iter()
-        .map(|tuple| [tuple.0, tuple.1])
-        .collect::<Vec<_>>();
-
-    println!("Flattens");
-    println!("{:?}", t1);
-
-    let f1 = t1.iter().flatten().flatten().collect::<Vec<_>>();
-    println!("{:?}", &f1);
-
-    // let f2 = f1.iter().flatten();
-    // println!("{:?}", f2);
-
-    println!("Arrays:");
-    println!("{:?}", arr);
-    println!("{:?}", res);
 
     let data = (0..size).map(|x| x as f64).zip(arr).collect::<Vec<_>>();
     let l1: Line = Line::new(data).style(plotlib::style::LineStyle::new().colour("#113355"));
