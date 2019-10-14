7163af1520fb14ae56dfa051aa4580cc0c1c7e89
Alex Komissarov <k.a.komissar@gmail.com>
2019-10-14 19:17:10 +0500

    print

--- src/main.cpp
+++ src/main.cpp
@@ -21,19 +21,18 @@ std::vector<double> addition(std::vector<double> arr) {
   return arr;
 }
 
+template <typename T>
+void print(std::vector<T> const& input) {
+  for (auto number : addition(input)) {
+    std::cout << number << ' ';
+  }
+}
+
 int main() {
   // std::vector<double> numbers;
   // std::generate_n(std::back_inserter(numbers), 10,
   //                 randomNumberBetween(0., 10.));
   std::vector<double> numbers = {1., 2., 3., 4., 5., 6., 7., 8., 9., 10.};
 
-  for (auto number : addition(numbers)) {
-    std::cout << number << ' ';
-  }
-}
-
-template <typename T>
-void print(std::vector<T> const& input) {
-  std::copy(input.begin(), input.end(),
-            std::experimental::make_ostream_joiner(std::cout, " "));
+  print(addition(numbers));
 }
